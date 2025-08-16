91c786781c80cfeb338b759b1468a23a66a9211b
unknown <amit.dcosta12@gmail.com>
2025-08-16 23:35:17 +0530

    Xdisplay task crio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  
+    const [firstName ,setFirstName] = useState("");
+    const[lastName, setLastName] = useState("");
+    const[fullName,setFullName] = useState("");
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+
+      if (firstName.trim()!== "" && lastName.trim()!==""){
+      setFullName(`Full Name: ${firstName} ${lastName}`);
+      }
+      else
+      {
+        setFullName("");
+
+      }
+    };
+    return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            id="firstName"
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            id="lastName"
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      
+      {fullName && <p>{fullName}</p>}
     </div>
   );
-}
+};
+
 
 export default App;
